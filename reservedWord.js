4afa52139482470acb0a3e25ce642b317f3163de
yoojanghun <yoojanghun@naver.com>
2025-06-20 13:38:40 +0900

    reserved word

---
+++ reservedWord.js
@@ -0,0 +1,6 @@
+// Reserved Word(예약어어) : 특별한 의미를 가지고 있어, 변수나 함수 이름 등으로 사용할 수 없는 단어
+
+let this = 'Hello!';    // SyntaxError
+let if = 123;           // SyntaxError
+let break = true;       // SyntaxError
+
